309731475e762adf4cab385bf9d58851b5849b0d
Tracy Tung <d820430@yahoo.com.tw>
2022-12-15 15:21:20 +0800

    fix path

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-  // publicPath: '/Vue_shop/dist/',
-  publicPath: process.env.NODE_ENV === 'production' ? '/Vue_shop/' : '/',
-
+  publicPath: '/Vue_shop/',
   css: {
     loaderOptions: {
       sass: {
