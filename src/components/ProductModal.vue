e668f95c17924776042e43fe43bc45e7201d4972
Tracy Tung <d820430@yahoo.com.tw>
2022-12-19 14:05:27 +0800

    程式碼fix

--- src/components/ProductModal.vue
+++ src/components/ProductModal.vue
@@ -221,7 +221,7 @@ export default {
       }).catch(err => {
         this.$swal({
           icon: 'error',
-          title: `${err.data.message}`
+          title: err.data.message
         })
       })
     }
